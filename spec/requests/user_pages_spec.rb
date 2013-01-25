2f4bb4d769953d7c7e05fce2b368868cbec19d3a
Lindsay Rattray <lindsayrattray@yahoo.com>
2013-01-25 14:30:36 +1100

    8.5.2 half done - about to split files

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,7 +33,6 @@ describe "User pages" do
         before { click_button submit }
         it { should have_selector('title',text: 'Sign up') }
         it { should have_content('error') } 
-        #it { should have_content('Password digest can\'t be blank') }
         it { should have_content('Name can\'t be blank') }
         it { should have_content('Email can\'t be blank') }
         it { should have_content('Email is invalid') }
@@ -44,22 +43,18 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      let(:user) { FactoryGirl.build(:user) }
+      before { valid_signup(user) }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
       describe "after saving the user" do
         before { click_button submit }
-        let(:user) { User.find_by_email('user@example.com') }
 
         it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
         it { should have_link('Sign out') }
       end
     end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,14 +7,21 @@ def valid_signin(user)
 end
 
 def valid_signup(user)
-  fill_in "Name",         with: "Example User"
-  fill_in "Email",        with: "user@example.com"
-  fill_in "Password",     with: "foobar"
-  fill_in "Confirmation", with: "foobar"
+  fill_in "Name",         with: user.name
+  fill_in "Email",        with: user.email
+  fill_in "Password",     with: user.password
+  fill_in "Confirmation", with: user.password_confirmation
 end
 
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
+  end
+
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
   end
 end
