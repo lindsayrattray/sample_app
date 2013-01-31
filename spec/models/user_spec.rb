ed8b637d7a6534d2f57b29493726aae0db76113e
Lindsay Rattray <lindsayrattray@yahoo.com>
2013-01-31 15:13:46 +1100

    Exercise 9.6.1 finished

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,6 +39,16 @@ describe User do
   it { should be_valid }
   it { should_not be_admin }
 
+  describe "accessible attributes" do
+    it "should not allow access to admin" do
+      expect do
+        @user.update_attributes(:admin => true)
+        #put user_path(user) + "?admin=1"
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end    
+  end
+
+
   describe "with admin attribute set to 'true'" do
     before do
       @user.save!
