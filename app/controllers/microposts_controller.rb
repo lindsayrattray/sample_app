0de4b08f5444cb4ab086da61e826497d3272cdeb
Lindsay Rattray <lindsayrattray@yahoo.com>
2013-02-07 14:18:11 +1100

    Add user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_filter :signed_in_user
+  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -11,9 +12,19 @@ class MicropostsController < ApplicationController
       # I don't like the above code but code below won't work with other pagination
       #@feed_items = current_user.feed.paginate(page: params[:page])
       render 'static_pages/home'
+      #redirect_to root_path
     end
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
+
+  private
+
+    def correct_user
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
 end
