593264dc0428f8af1a858bdb98ab15106e93f63e
Lindsay Rattray <lindsayrattray@yahoo.com>
2013-02-08 16:32:26 +1100

    Up to Listing 11.23

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,9 @@ describe "Static pages" do
 
   subject { page }
 
+  let(:heading) { 'Sample App' }
+  let(:page_title) { '' }
+
   shared_examples_for "all static pages" do
     it { should have_selector('h1',    text: heading) }
     it { should have_selector('title', text: full_title(page_title)) }
@@ -11,8 +14,6 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-    let(:heading) { 'Sample App' }
-    let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
     it { should_not have_selector 'title', text: '| Home' }
@@ -30,6 +31,17 @@ describe "Static pages" do
         user.feed.each do |item|
           page.should have_selector("li##{item.id}", text: item.content)
         end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
     end
   end
